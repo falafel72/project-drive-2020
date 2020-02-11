335025a17e27e6b284f3da654db78a69ca9a17eb
Falafel72 <kylehu2000@gmail.com>
2020-02-11 14:18:00 -0800

    no need to check whether the folder is empty

--- scripts/pull_packages.bash
+++ scripts/pull_packages.bash
@@ -4,16 +4,11 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # get UBUNTU_CODENAME, ROS_DISTRO, REPO_DIR, CATKIN_DIR
 source $SCRIPT_DIR/identify_environment.bash
 
-if [ "$(ls $HOME/catkin_ws/src/)" ]; then
-    echo "git repo already installed"
-else
-    echo "repo not detected"
-    cd "$HOME/catkin_ws/src"
-    git clone https://github.com/falafel72/project-drive-2020.git .
+cd "$HOME/catkin_ws/src"
+git clone https://github.com/falafel72/project-drive-2020.git .
 #    cd "$HOME/catkin_ws"
 #    catkin build --no-status
 #    echo "Package built successfully"
-fi
 
 # Add scripts to build each package here
 
