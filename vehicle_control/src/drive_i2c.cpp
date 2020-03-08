d4fbc54aacdaefc9c25231c2961c952e4f3cff36
falafel72 <kshu@ucsd.edu>
2020-03-08 23:38:59 +0000

    added code for i2c

--- vehicle_control/src/drive_i2c.cpp
+++ vehicle_control/src/drive_i2c.cpp
@@ -5,20 +5,31 @@
 #define I2C_ADDRESS 0x40
 #define I2C_BUS 1
 
+#define ESC_CHANNEL 1
+#define SERVO_CHANNEL 2
+
+PCA9685 *controller = new PCA9685(I2C_BUS,I2C_ADDRESS);
+double map(double input, double inrange_m, double inrange_M, double outrange_m, double outrange_M) {
+	double output = (input - inrange_m)/(inrange_M - inrange_m) * (outrange_M - outrange_m) + outrange_m;
+	return output;
+}
+
 
 void driveCallback(const project_drive_msgs::CarParams::ConstPtr &msg) {
-	PCA9685 *controller = new PCA9685(I2C_BUS,I2C_ADDRESS);
-	// set up pwm frequency
-	
 	// convert angle and velocity to pwm values
-	
+	int servo_pulse_len = (int) map((double) msg->angle, 0.0, 180.0, 0.0, 4095.0);	
+	int esc_pulse_len = (int) map((double) msg->velocity, -100.0, 100.0, 0.0, 4095.0);
 	// send i2c command	
+	controller->setPWM(SERVO_CHANNEL, servo_pulse_len);
+	controller->setPWM(ESC_CHANNEL, esc_pulse_len);
 }
 
 int main(int argc, char **argv){		
 	ros::init(argc, argv, "drive_i2c");
 	ros::NodeHandle n;
 
+	// set up pwm frequency
+	controller->setPWMFreq(100);
 	ros::Subscriber sub = n.subscribe("drive_params", 1000, driveCallback);
 
 	ros::spin();
