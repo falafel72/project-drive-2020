009852d58318f396b56413b63aab20d2b1c0d7ad
Tianyue-Zhao <zhaotianyue@outlook.com>
2020-07-04 19:38:08 -0700

    Completed car_state prediction of paths

--- local_algorithm/scripts/decider_sim.py
+++ local_algorithm/scripts/decider_sim.py
@@ -43,11 +43,11 @@ CONFIG_FILE = "./config.json"
 MAP_TOPIC = "/map"
 PATH_TOPIC = "/green_path"
 # These are set via command line
-# FRAME_RATE = 10
-# CONTROL_TOPIC = "/drive"
-# LASER_TOPIC = "/scan"
-# ODOM_TOPIC = "/odom"
-# PUBLISH_PATH = True
+FRAME_RATE = 10
+CONTROL_TOPIC = "/drive"
+LASER_TOPIC = "/scan"
+ODOM_TOPIC = "/odom"
+PUBLISH_PATH = True
 # PID drive publisher
 pid_pub = rospy.Publisher("/drive", AckermannDriveStamped, queue_size=10)
 points = []
