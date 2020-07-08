3737f83b60d56230450e0b41be4501ec7e0283ca
Tianyue-Zhao <zhaotianyue@outlook.com>
2020-07-07 23:51:02 -0700

    Quick fix

--- local_algorithm/scripts/main.py
+++ local_algorithm/scripts/main.py
@@ -4,5 +4,5 @@ import time
 
 if __name__ == "__main__":
     time.sleep(1)
-    #control.decider_sim.pid_handle()
-    control.decider_sim.cost_handle(False, False, 10)
+    control.decider_sim.pid_handle()
+    #control.decider_sim.cost_handle(False, False, 10)
