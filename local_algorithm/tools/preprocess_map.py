258f1f2ac2bc2fec37c261fd8762110bab622a45
Tianyue-Zhao <zhaotianyue@outlook.com>
2020-07-08 18:20:41 -0700

    Added blur

---
+++ local_algorithm/tools/preprocess_map.py
@@ -0,0 +1,8 @@
+import cv2
+import numpy as np
+map_img = cv2.imread('../scripts/path_planning/tools/berlin.png')
+map_img = map_img[:,:,0]
+map_img = cv2.blur(map_img, (5,5))
+threshold, map_img = cv2.threshold(map_img, 240, 255, cv2.THRESH_BINARY)
+np.savetxt('berlin_blurred.txt', map_img)
+cv2.imwrite('berlin_blurred.png', map_img)
